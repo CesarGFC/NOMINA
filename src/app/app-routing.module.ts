92976d15a95eeefc3196a61359bbc974d4dcbf82
Arturo Larios <arallariosqu@ittepic.edu.mx>
2020-03-04 16:18:36 -0700

    finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'view-person',
+    loadChildren: () => import('./view-person/view-person.module').then( m => m.ViewPersonPageModule)
+  },
+  {
+    path: 'edit-person',
+    loadChildren: () => import('./edit-person/edit-person.module').then( m => m.EditPersonPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,79 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  paysheets: any = [{
+    rfc: 'JUJU123456532',
+    password: 1234,
+    name: {
+      firstName: 'Juan',
+      lastName: 'Perez',
+      fullName: 'Juan Perez'
+    },
+    salary: 50000,
+    type: 'Base',
+    active: true,
+    email: 'je@hotmail.com',
+    initialDate: '1992/12/12'
+  },
+  {
+    rfc: 'AN123LO123',
+    password: 4321,
+    name: {
+      firstName: 'Joy',
+      lastName: 'Gonzalez',
+      fullName: 'Joy Gonzalez'
+    },
+    salary: 100000,
+    type: 'Confianza',
+    active: false,
+    email: 'jG@hotmail.com',
+    initialDate: '1995/12/12'
+  },
+  {
+    rfc: '12345689llkj',
+    password: 3333,
+    name: {
+      firstName: 'Anaí',
+      lastName: 'Gonzalez',
+      fullName: 'Anaí Gonzalez'
+    },
+    salary: 1000,
+    type: 'Confianza',
+    active: true,
+    email: 'ag@hotmail.com',
+    initialDate: '1995/12/12'
+  },
+  {
+    rfc: 'LAQA971222HNT',
+    password: 123,
+    name: {
+      firstName: 'Arturo',
+      lastName: 'Larios',
+      fullName: 'Arturo Larios'
+    },
+    salary: 1000,
+    type: 'Confianza',
+    active: false,
+    email: 'ak@hotmail.com',
+    initialDate: '1995/12/12'
+  }
+];
 
+  constructor(private router: Router) {}
+
+  view(paysheet: any): void 
+  {
+    const extras: NavigationExtras = {
+      queryParams: {
+        special: JSON.stringify(paysheet)
+      }
+    };
+
+    this.router.navigate(['/view-person'], extras);
+  }
+
+  edit()
+  {
+    this.router.navigate(['/edit-person']);
+  }
 }
